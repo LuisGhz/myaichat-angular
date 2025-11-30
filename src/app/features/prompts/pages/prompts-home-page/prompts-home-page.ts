40f66aa7f92ddc140abb3b3bb76cb057550dcac6
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-11-30 14:16:15 -0600

    Create prompts home page

---
+++ src/app/features/prompts/pages/prompts-home-page/prompts-home-page.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prompts-home-page',
+  imports: [],
+  templateUrl: './prompts-home-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PromptsHomePage {
+
+}
