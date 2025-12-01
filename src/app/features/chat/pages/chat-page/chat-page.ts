a5b9436f902a41ecab1ef361f19ecfeb6979b29c
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-01 14:09:00 -0600

    Refactor

--- src/app/features/chat/pages/chat-page/chat-page.ts
+++ src/app/features/chat/pages/chat-page/chat-page.ts
@@ -3,29 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
-import { LayoutService } from '../../../../core/services/layout.service';
+import { LayoutService } from '@core/services/layout.service';
+import { InputMessage } from '@chat/components/input/input-message';
 
 @Component({
   selector: 'app-chat-view',
-  imports: [FormsModule, NzButtonModule, NzIconModule, NzInputModule],
+  imports: [FormsModule, NzButtonModule, NzIconModule, NzInputModule, InputMessage],
   templateUrl: './chat-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChatPage {
   protected readonly layoutService = inject(LayoutService);
-  protected messageText = '';
-
-  onSend(): void {
-    if (this.messageText.trim()) {
-      console.log('Sending:', this.messageText);
-      this.messageText = '';
-    }
-  }
-
-  onKeydown(event: KeyboardEvent): void {
-    if (event.key === 'Enter' && !event.shiftKey) {
-      event.preventDefault();
-      this.onSend();
-    }
-  }
 }
