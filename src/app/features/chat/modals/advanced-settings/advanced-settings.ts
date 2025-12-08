93c99bbb7bca5f0ae8f0fd8d131e1bf2a27746c7
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-08 07:56:01 -0600

    Refactor

--- src/app/features/chat/modals/advanced-settings/advanced-settings.ts
+++ src/app/features/chat/modals/advanced-settings/advanced-settings.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectionStrategy, Component, computed, inject, input, output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  DestroyRef,
+  effect,
+  inject,
+  input,
+  OnInit,
+  output,
+} from '@angular/core';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 import {
@@ -12,6 +22,7 @@ import { dispatch, select } from '@ngxs/store';
 import { ChatStore } from '@st/chat/chat.store';
 import { ChatActions } from '@st/chat/chat.actions';
 import { ChatApi } from '@chat/services/chat-api';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 interface AdvancedSettingsFormModel {
   maxTokens: FormControl<number>;
@@ -24,7 +35,7 @@ interface AdvancedSettingsFormModel {
   templateUrl: './advanced-settings.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdvancedSettings {
+export class AdvancedSettings implements OnInit {
   isVisible = input.required<boolean>();
   closeModal = output<void>();
 
@@ -33,16 +44,11 @@ export class AdvancedSettings {
   #ops = select(ChatStore.getOps);
   #currentChatId = select(ChatStore.getCurrentChatId);
   #setOps = dispatch(ChatActions.SetOps);
-  form = this.#fb.group<AdvancedSettingsFormModel>({
-    maxTokens: this.#fb.nonNullable.control<number>(this.#ops().maxTokens, [
-      Validators.min(100),
-      Validators.max(8000),
-    ]),
-    temperature: this.#fb.nonNullable.control<number>(this.#ops().temperature, [
-      Validators.min(0),
-      Validators.max(1),
-    ]),
-  });
+  #destroyRef = inject(DestroyRef);
+  #minTokens = 100;
+  #maxTokens = 8000;
+  #minTemperature = 0;
+  #maxTemperature = 1;
 
   maxTokens = computed(() => this.#ops().maxTokens);
   maxTokensTimeout: number | null = null;
@@ -50,13 +56,45 @@ export class AdvancedSettings {
   temperatureTimeout: number | null = null;
   debounceDelay = 500;
 
+  maxTokensErrors = computed(() => this.form.controls.maxTokens.errors);
+  temperatureErrors = computed(() => this.form.controls.temperature.errors);
+  form = this.#fb.group<AdvancedSettingsFormModel>({
+    maxTokens: this.#fb.nonNullable.control<number>(0, [
+      Validators.min(this.#minTokens),
+      Validators.max(this.#maxTokens),
+    ]),
+    temperature: this.#fb.nonNullable.control<number>(0, [
+      Validators.min(this.#minTemperature),
+      Validators.max(this.#maxTemperature),
+    ]),
+  });
+
+  constructor() {
+    effect(() => {
+      if (this.isVisible())
+        this.form.patchValue(
+          {
+            maxTokens: this.maxTokens(),
+            temperature: this.temperature(),
+          },
+          { emitEvent: false },
+        );
+    });
+  }
+
   ngOnInit(): void {
-    this.form.controls.maxTokens.valueChanges.subscribe((value) => {
-      if (value !== null) this.onMaxTokensChange(value);
-    });
-    this.form.controls.temperature.valueChanges.subscribe((value) => {
-      if (value !== null) this.onTemperatureChange(value);
-    });
+    this.form.controls.maxTokens.valueChanges
+      .pipe(takeUntilDestroyed(this.#destroyRef))
+      .subscribe((value) => {
+        if (this.form.controls.maxTokens.invalid) return;
+        if (value !== null) this.onMaxTokensChange(value);
+      });
+    this.form.controls.temperature.valueChanges
+      .pipe(takeUntilDestroyed(this.#destroyRef))
+      .subscribe((value) => {
+        if (this.form.controls.temperature.invalid) return;
+        if (value !== null) this.onTemperatureChange(value);
+      });
   }
 
   onMaxTokensChange(value: number): void {
