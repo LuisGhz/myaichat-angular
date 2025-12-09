0195b14dc4fa828e896f57c0ff716289fda6caf6
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-09 14:28:36 -0600

    feat: add mobile menu toggle button to header component

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,11 +1,16 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 import { filter } from 'rxjs';
+import { dispatch, select } from '@ngxs/store';
+import { AppStore } from '@st/app/app.store';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { AppActions } from '@st/app/app.actions';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, NzIconModule],
   templateUrl: './header.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -13,6 +18,9 @@ export class Header {
   #title = inject(Title);
   #router = inject(Router);
   headerTitle = signal('');
+  unCollapseSideNav = dispatch(AppActions.UnCollapseSidebar);
+  isCollapsed = select(AppStore.sidebarCollapsed);
+  isMobile = select(AppStore.isMobile);
 
   ngOnInit(): void {
     this.#router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
