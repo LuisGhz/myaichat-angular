88c006c16fa123c68a02543be5fea319d3205dd6
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-11 15:30:17 -0600

    feat(admin): add guestAccess field to CreateModelReqModel

--- src/app/features/admin/models/dtos/create-model.model.ts
+++ src/app/features/admin/models/dtos/create-model.model.ts
@@ -6,6 +6,7 @@ export interface CreateModelReqModel {
   name: string;
   shortName: string;
   value: string;
+  guestAccess: boolean;
   link: string;
   price: PriceModel;
   metadata: MetadataModel;
