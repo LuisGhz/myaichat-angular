84d86516606111a2a7c30291435da216ff1c9688
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2026-01-12 22:27:10 -0600

    Update package name

--- src/app/core/components/more/more.ts
+++ src/app/core/components/more/more.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component, output } from '@angular/core';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzDropdownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-more',
-  imports: [NzDropDownModule, NzIconModule],
+  imports: [NzDropdownModule, NzIconModule],
   templateUrl: './more.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
